350de959a9130fb370f2e7b2d6c251ceb53ae7cb
andrey.ko-ws <akolomentsev@machinezone.com>
2015-12-14 15:57:38 -0800

    update ActionExtensions

--- xutils.common/sources/ActionExtensions.cs
+++ xutils.common/sources/ActionExtensions.cs
@@ -4,9 +4,23 @@ using System.Threading;
 namespace xutils {
 
 	public static class ActionExtensions {
+		public static void InvokeSafe(this Action action) {
+			if (action == null) {
+				return;
+			}
+			foreach (Action a in action.GetInvocationList()) {
+				try {
+					a();
+				} catch (Exception exn) {
+					if (!FastFail.Swallow(exn)) {
+						throw;
+					}
+				}
+			}
+		}
+
 		public static void InvokeSafe<T>(this Action<T> action, T p1) {
-			var cb = Volatile.Read(ref action);
-			if (cb == null) {
+			if (action == null) {
 				return;
 			}
 			foreach (Action<T> a in action.GetInvocationList()) {
@@ -20,8 +34,7 @@ namespace xutils {
 			}
 		}
 		public static void InvokeSafe<T1, T2>(this Action<T1, T2> action, T1 p1, T2 p2) {
-			var cb = Volatile.Read(ref action);
-			if (cb == null) {
+			if (action == null) {
 				return;
 			}
 			foreach (Action<T1, T2> a in action.GetInvocationList()) {
@@ -35,8 +48,7 @@ namespace xutils {
 			}
 		}
 		public static void InvokeSafe<T1, T2, T3>(this Action<T1, T2, T3> action, T1 p1, T2 p2, T3 p3) {
-			var cb = Volatile.Read(ref action);
-			if (cb == null) {
+			if (action == null) {
 				return;
 			}
 			foreach (Action<T1, T2, T3> a in action.GetInvocationList()) {
